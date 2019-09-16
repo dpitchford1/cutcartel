c2ad7267dac4e11e6294c65ba7f37b8024f2550c
D Pitchford <pitchford.dylan@gmail.com>
2019-09-16 10:55:20 -0400

    Initial Upload
    Adding Cut Cartel to GitHub

---
+++ media/js/core/core.js
@@ -0,0 +1,234 @@
+﻿
+
+/* application wrapper
+-------------------------------------------------------------------------------------------------------------------------- */
+var cutcartel = cutcartel || {};
+window.cutcartel = (function (window, document, ccwrapper) {
+
+
+    window.onscroll = function changeClass(){
+        var scrollPosY = window.pageYOffset | document.body.scrollTop;
+        var navBar = document.getElementById('global-header');
+
+        if(scrollPosY > 100) {
+              navBar.className = ('global-header is-sticky');
+        } else if(scrollPosY <= 100) {
+             navBar.className =  ('global-header');
+        }
+
+    }
+
+    /* load up a couple helpers
+    ---------------------------------------------- */
+
+    /* set atributes */
+    function setAttributes(el, attrs) {
+        for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+        }
+    };
+
+
+    /* set up for lazyloading images and video on small screens
+    ---------------------------------------------- */
+    var Utils = {
+        q : function (q, res) {
+            if (doc.querySelectorAll) {
+                res = doc.querySelectorAll(q);
+            } else {
+                a = doc.styleSheets[0] || doc.createStyleSheet();
+                a.addRule(q, 'f:b');
+                for (var l = d.all, b = 0, c = [], f = l.length;b < f;b++)
+                l[b].currentStyle.f && c.push(l[b]);
+          
+                a.removeRule(0);
+                res = c;
+            }
+            return res;
+        }
+    };
+    function initImages() {
+        "use strict";
+        var lazy = Utils.q('[data-src]'),
+            //imgBos = Utils.q('[data-bos]'),
+            imgAlt = Utils.q('[data-alt]'),
+            imgWidth = Utils.q('[data-width]'),
+            imgHeight = Utils.q('[data-height]');
+        
+        for (var i = 0; i < lazy.length; i++) {
+            var source = lazy[i].getAttribute('data-src'),
+                alt = imgAlt[i].getAttribute('data-alt'),
+                width = imgWidth[i].getAttribute('data-width'),
+                height = imgHeight[i].getAttribute('data-height'),
+                img = new Image();
+            
+            //create the image & alt
+            img.src = source;
+            img.alt = alt;
+            img.width = width;
+            img.height = height;
+            //insert it inside of the span
+            lazy[i].insertBefore(img, lazy[i].firstChild);
+            imgAlt[i].appendChild(img);
+            imgWidth[i].appendChild(img);
+            imgHeight[i].appendChild(img);
+        }
+    }
+    // end lazy images
+
+
+    /* touch - pointer check
+    ---------------------------------------------- */
+    function touchEvents() {
+    
+        var supportsTouch = false;
+        if ('ontouchstart' in window) {
+            //iOS & android
+            supportsTouch = true;
+
+            doc.documentElement.className = doc.documentElement.className.replace(/\bno-touch\b/g, '') + ' has-touch';
+            //alert('boing! iOS & Android');
+
+            /* A fix for the iOS orientationchange zoom bug. Script by @scottjehl, rebound by @wilto. MIT / GPLv2 License.  */
+            (function () {
+
+                // This fix addresses an iOS bug, so return early if the UA claims it's something else.
+                var ua = navigator.userAgent;
+                if (!(/iPhone|iPad|iPod/.test(navigator.platform) && /OS [1-5]_[0-9_]* like Mac OS X/i.test(ua) && ua.indexOf("AppleWebKit") > -1)) {
+                    return;
+                }
+
+                if (!doc.querySelector) { return; }
+
+                var meta = doc.querySelector("meta[name=viewport]"),
+                    initialContent = meta && meta.getAttribute("content"),
+                    disabledZoom = initialContent + ",maximum-scale=1",
+                    enabledZoom = initialContent + ",maximum-scale=10",
+                    enabled = true,
+                    x, y, z, aig;
+
+                if (!meta) { return; }
+
+                function restoreZoom() {
+                    meta.setAttribute("content", enabledZoom);
+                    enabled = true;
+                }
+
+                function disableZoom() {
+                    meta.setAttribute("content", disabledZoom);
+                    enabled = false;
+                }
+
+                function checkTilt(e) {
+                    aig = e.accelerationIncludingGravity;
+                    x = Math.abs(aig.x);
+                    y = Math.abs(aig.y);
+                    z = Math.abs(aig.z);
+
+                    // If portrait orientation and in one of the danger zones
+                    if ((!w.orientation || w.orientation === 180) && (x > 7 || ((z > 6 && y < 8 || z < 8 && y > 6) && x > 5))) {
+                        if (enabled) {
+                            disableZoom();
+                        }
+                    }
+                    else if (!enabled) {
+                        restoreZoom();
+                    }
+                }
+
+                w.addEventListener("orientationchange", restoreZoom, false);
+                w.addEventListener("devicemotion", checkTilt, false);
+
+            })(this);
+            // end orientation fix
+
+        } else if (window.navigator.msPointerEnabled) {
+            //Win8
+            supportsTouch = true;
+
+            doc.documentElement.className = doc.documentElement.className.replace(/\bno-touch\b/g, '') + ' has-touch';
+            //alert('bang!!! windows8');
+
+        } else if (supportsTouch === false) {
+
+            doc.documentElement.className = doc.documentElement.className.replace(/\bno-touch\b/g, '') + ' no-touch';
+            //alert('cool beans! do not support touch');
+        }
+    }
+    // end touchEvents
+
+    /* Lazy Load Videos
+    -------------------------------------------------------------------------------------------------------------------------- */
+    function lazyvids() {
+        function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+        r(function(){
+            if(!document.getElementsByClassName) {
+                // IE8 support
+                var getElementsByClassName = function(node, classname) {
+                    var a = [],
+                        re = new RegExp('(^| )'+classname+'( |$)'),
+                        els = node.getElementsByTagName("*");
+
+                    for(var i=0,j=els.length; i<j; i++)
+                        if(re.test(els[i].className))a.push(els[i]);
+                    return a;
+                }
+                var videos = getElementsByClassName(document.body,"youtube");
+            }
+            else {
+                var videos = document.getElementsByClassName("youtube");
+            }
+
+            var nb_videos = videos.length;
+            for (var i=0; i<nb_videos; i++) {
+                // Based on the YouTube ID, we can easily find the thumbnail image
+                videos[i].style.backgroundImage = 'url(http://i.ytimg.com/vi/' + videos[i].id + '/sddefault.jpg)';
+
+                // Overlay the Play icon to make it look like a video player
+                var play = document.createElement("div");
+                play.setAttribute("class","play");
+                videos[i].appendChild(play);
+
+                videos[i].onclick = function() {
+                    // Create an iFrame with autoplay set to true
+                    var iframe = document.createElement("iframe"),
+                        iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+
+                    if (this.getAttribute("data-params")) iframe_url+='&'+this.getAttribute("data-params");
+                    iframe.setAttribute("src",iframe_url);
+                    iframe.setAttribute("frameborder",'0');
+                    iframe.setAttribute('allowfullscreen', 1);
+                    //iframe.setAttribute("allowfullscreeen");
+
+                    // The height and width of the iFrame should be the same as parent
+                    // iframe.style.width  = this.style.width;
+                    // iframe.style.height = this.style.height;
+
+                    // Replace the YouTube thumbnail with YouTube Player
+                    this.parentNode.replaceChild(iframe, this);
+                }
+            }
+        });
+
+    };
+
+
+    /* load'em up! & execute!
+    -------------------------------------------------------------------------------------------------------------------------- */
+
+    /* always load touch events */
+    touchEvents();
+
+    if (doc.querySelectorAll('.lazy-img')) {
+
+        if (window.matchMedia('only screen and (min-width: 299px)').matches) {
+            addLoadEvent(initImages);
+        }
+
+    }
+
+    if (doc.querySelectorAll('.youtube')) {
+            addLoadEvent(lazyvids);
+    }
+
+})(this, this.document);
---
+++ media/js/core/utils.js
@@ -0,0 +1,29 @@
+﻿var appSetup = appSetup || {};
+window.appSetup = (function (window, document, undefined) {
+
+    // dynamically load enhanced css
+    // if (document.querySelector && document.addEventListener) {
+    //     var head = document.getElementsByTagName("head")[0],
+    //         cssUrl = "/media/css/core/enhance.css?v=1395690932944",
+    //         link = document.createElement("link");
+    
+    //         link.href = cssUrl; link.media = "only screen and (min-width: 10em)"; link.rel = "stylesheet"; document.head.appendChild(link);
+    // };
+    
+    /* FONT LOADING SCRIPT -- USES LOCALSTORAGE FOR CACHING
+    -------------------------------------------------------------------------------------------------------------------------- */
+    !function(){function e(e,t,n){e.addEventListener?e.addEventListener(t,n,!1):e.attachEvent&&e.attachEvent("on"+t,n)}function t(e){return window.localStorage&&localStorage.font_css_cache&&localStorage.font_css_cache_file===e}function n(){if(window.localStorage&&window.XMLHttpRequest)if(t(o))a(localStorage.font_css_cache);else{var n=new XMLHttpRequest;n.open("GET",o,!0),e(n,"load",function(){4===n.readyState&&(a(n.responseText),localStorage.font_css_cache=n.responseText,localStorage.font_css_cache_file=o)}),n.send()}else{var c=document.createElement("link");c.href=o,c.rel="stylesheet",c.type="text/css",document.getElementsByTagName("head")[0].appendChild(c),document.cookie="font_css_cache"}}function a(e){var t=document.createElement("style");t.innerHTML=e,document.getElementsByTagName("head")[0].appendChild(t)}var o="/cutcartel/media/css/build/fonts.min.css";window.localStorage&&localStorage.font_css_cache||document.cookie.indexOf("font_css_cache")>-1?n():e(window,"load",n)}();
+
+    if (doc.querySelector && doc.addEventListener) {
+
+        /* IE10 Mobile bug fix
+        -------------------------------------------------------------------------------------------------------------------------- */
+        (function() { if (navigator.userAgent.match(/IEMobile\/10\.0/)) { var msViewportStyle = doc.createElement("style"); msViewportStyle.appendChild( doc.createTextNode("@-ms-viewport{width:auto!important}") ); doc.getElementsByTagName("head")[0].appendChild(msViewportStyle); } })();
+    }
+
+    /* matchMedia() polyfill  
+    Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license
+    -------------------------------------------------------------------------------------------------------------------------- */
+    window.matchMedia=window.matchMedia||function(e,t){"use strict";var n,r=e.documentElement,i=r.firstElementChild||r.firstChild,s=e.createElement("body"),o=e.createElement("div");o.id="mq-test-1";o.style.cssText="position:absolute;top:-100em";s.style.background="none";s.appendChild(o);return function(e){o.innerHTML='­<style media="'+e+'"> #mq-test-1 { width: 42px; }</style>';r.insertBefore(s,i);n=o.offsetWidth===42;r.removeChild(s);return{matches:n,media:e}}}(document);
+
+})(this, this.document);
